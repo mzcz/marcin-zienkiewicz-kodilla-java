8f85e45de81cf263515029ddd24906c5a6969743
mzcz <mzcz@wp.pl>
2017-10-21 20:23:41 +0200

    Instream

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,19 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.OptionalDouble;
+
 public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage(){
+        //Given
+        int[] tab = {1,2,3,4,-1,-2,-3,-4,1,2,3,4,5,-15,1,2,3,4,5,50};
+        //When
+        double averageTabOFIntInt = ArrayOperations.getAverage(tab);
+        //Then
+        Assert.assertEquals(3.25, averageTabOFIntInt,0.0);
+    }
 }
